36796064a314732488ba1e18c8dc748dd29292f9
heyjanac <heyjanac@gmail.com>
2020-08-31 00:45:59 -0300

    secao6 implementando calculadora

--- src/main/java/br/com/heyjanac/controller/MathController.java
+++ src/main/java/br/com/heyjanac/controller/MathController.java
@@ -1,21 +1,67 @@
-package br.com.heyjanac;
+package br.com.heyjanac.controller;
 
-import java.util.concurrent.atomic.AtomicLong;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.heyjanac.exception.UnsuportedMathOperationException;
+import br.com.heyjanac.math.SimpleMath;
+import br.com.heyjanac.request.converters.NumberConverters;
+
 @RestController
-public class GreetingController {
+public class MathController {
 	
-	private static final String template = "Hello, %s!";
-			
-	private final AtomicLong counter = new AtomicLong();
-	
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template,  name));
+	private SimpleMath math = new SimpleMath();
+
+	@RequestMapping(value="/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double sum(@PathVariable ("numberOne") String numberOne, @PathVariable ("numberTwo") String numberTwo) throws Exception {
+		if(!NumberConverters.isNumeric(numberOne) || !NumberConverters.isNumeric(numberTwo)){
+			throw new UnsuportedMathOperationException("Por favor, defina um valor numerico!");
+		}
+		return math.sum(NumberConverters.convertToDouble(numberOne), NumberConverters.convertToDouble(numberTwo));
 	}
+	
+	@RequestMapping(value="/subtraction/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double subtraction(@PathVariable ("numberOne") String numberOne, @PathVariable ("numberTwo") String numberTwo) throws Exception {
+		if(!NumberConverters.isNumeric(numberOne) || !NumberConverters.isNumeric(numberTwo)){
+			throw new UnsuportedMathOperationException("Por favor, defina um valor numerico!");
+		}
+		return math.subtraction(NumberConverters.convertToDouble(numberOne) , NumberConverters.convertToDouble(numberTwo));
+	}
+	
+	@RequestMapping(value="/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double multiplication(@PathVariable ("numberOne") String numberOne, @PathVariable ("numberTwo") String numberTwo) throws Exception {
+		if(!NumberConverters.isNumeric(numberOne) || !NumberConverters.isNumeric(numberTwo)){
+			throw new UnsuportedMathOperationException("Por favor, defina um valor numerico!");
+		}
+		return math.multiplication(NumberConverters.convertToDouble(numberOne) , NumberConverters.convertToDouble(numberTwo));
+	}
+	
+	@RequestMapping(value="/division/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double division(@PathVariable ("numberOne") String numberOne, @PathVariable ("numberTwo") String numberTwo) throws Exception {
+		if(!NumberConverters.isNumeric(numberOne) || !NumberConverters.isNumeric(numberTwo)){
+			throw new UnsuportedMathOperationException("Por favor, defina um valor numerico!");
+		}
+		return math.division(NumberConverters.convertToDouble(numberOne) , NumberConverters.convertToDouble(numberTwo));
+	}
+	
+	@RequestMapping(value="/mean/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+	public Double mean(@PathVariable ("numberOne") String numberOne, @PathVariable ("numberTwo") String numberTwo) throws Exception {
+		if(!NumberConverters.isNumeric(numberOne) || !NumberConverters.isNumeric(numberTwo)){
+			throw new UnsuportedMathOperationException("Por favor, defina um valor numerico!");
+		}
+		return math.mean(NumberConverters.convertToDouble(numberOne) , NumberConverters.convertToDouble(numberTwo));
+	}
+	
+	@RequestMapping(value="/squareRoot/{number}", method = RequestMethod.GET)
+	public Double squareRoot(@PathVariable ("number") String number) throws Exception {
+		if(!NumberConverters.isNumeric(number)){
+			throw new UnsuportedMathOperationException("Por favor, defina um valor numerico!");
+		}
+		return math.squareRoot(NumberConverters.convertToDouble(number));
+	}
+
+	
 
 }
---
+++ src/main/java/br/com/heyjanac/math/SimpleMath.java
@@ -0,0 +1,31 @@
+package br.com.heyjanac.math;
+
+public class SimpleMath {
+	
+	public Double sum(Double numberOne, Double numberTwo) {
+		return numberOne + numberTwo;
+	}
+	
+	public Double subtraction(Double numberOne, Double numberTwo) {
+		return numberOne - numberTwo;
+	}
+	
+	
+	public Double division(Double numberOne, Double numberTwo) {
+		return numberOne / numberTwo;
+	}
+	
+	public Double mean(Double numberOne, Double numberTwo) {
+		return (numberOne + numberTwo)/2;
+	}
+	
+	public Double squareRoot(Double number) {
+		return (Double) Math.sqrt(number);
+	}
+
+	public Double multiplication(Double numberOne, Double numberTwo) {
+		return numberOne * numberTwo;
+	}
+
+	
+}
