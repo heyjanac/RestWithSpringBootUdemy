9190f908f5b82d7684bdfc65791b960b4b612c7e
heyjanac <heyjanac@gmail.com>
2020-08-30 21:06:14 -0300

    Hello SpringBoot and REST Udemy

---
+++ rest-with-springboot-udemy/src/test/java/br/com/heyjanac/restwithspringbootudemy/RestWithSpringbootUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.heyjanac.restwithspringbootudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringbootUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
